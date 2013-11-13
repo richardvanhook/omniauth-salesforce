b050495a3d5788c099f648923b13068ffe361a37
mintotsai <mintotsai@gmail.com>
2013-11-13 14:54:34 -0600

    Fix for can't convert Bignum into String

--- lib/omniauth/strategies/salesforce.rb
+++ lib/omniauth/strategies/salesforce.rb
@@ -33,7 +33,7 @@ module OmniAuth
 
       def auth_hash
         signed_value = access_token.params['id'] + access_token.params['issued_at']
-        raw_expected_signature = OpenSSL::HMAC.digest('sha256', options.client_secret, signed_value)
+        raw_expected_signature = OpenSSL::HMAC.digest('sha256', options.client_secret.to_s, signed_value)
         expected_signature = Base64.strict_encode64 raw_expected_signature
         signature = access_token.params['signature']
         fail! "Salesforce user id did not match signature!" unless signature == expected_signature
